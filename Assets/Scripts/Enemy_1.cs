0a35d884bf0789e48b6330b84fdcc68e9a129eda
alvardsoler <alsoler@ucm.es>
2016-04-20 18:14:12 +0200

    Enemigos hacen ataques diferentes. Enemy_4 deberia destruirse por partes y quita la vida al padre.

---
+++ Assets/Scripts/Enemy_1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_1 : Enemy
+{
+	public float waveFrequency = 2;
+
+	public float waveWidth = 4;
+	public float waveRotY = 45;
+
+	private float x0 = -12345;
+	private float birthTime;
+	// Use this for initialization
+	void Start ()
+	{
+		// Set x0 to the initial x position of Enemy_1
+		// This works fine because the position will have already
+		//   been set by Main.SpawnEnemy() before Start() runs
+		//   (though Awake() would have been too early!).
+		// This is also good because there is no Start() method
+		//   on Enemy
+		x0 = pos.x;
+		birthTime = Time.time;
+	}
+
+	public override void Move ()
+	{
+		// Because pos is a property, you can't directly set pos.x
+		//   so get the pos as an editable Vector3
+		Vector3 tempPos = pos;
+		// theta adjusts based on time
+		float age = Time.time - birthTime;
+		float theta = Mathf.PI * 2 * age / waveFrequency;
+		float sin = Mathf.Sin (theta);
+		tempPos.x = x0 + waveWidth * sin;
+		pos = tempPos;
+		// rotate a bit about y
+		Vector3 rot = new Vector3 (0, sin * waveRotY, 0);
+		this.transform.rotation = Quaternion.Euler (rot);
+		// base.Move() still handles the movement down in y
+		base.Move ();
+	}
+
+}
